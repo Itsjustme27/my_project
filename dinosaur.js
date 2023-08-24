a787006809baf822c787a23842510949b4e7e645
Itsjustme27 <122108318+Itsjustme27@users.noreply.github.com>
2023-08-24 18:08:25 +0545

    Add files via upload

---
+++ dinosaur.js
@@ -0,0 +1,8 @@
+
+const button = document.getElementById('button');
+button.addEventListener('click', video);
+
+function video(){
+    window.open('https://www.youtube.com/watch?v=5s8fs_j2xlY&ab_channel=AnastasiaNikulina/', '_blank');
+}
+
